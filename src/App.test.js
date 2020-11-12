0b64cfa8050c1ed81c500c39b594bf52a5fa148c
Olly Swanson <olly.swanson95@gmail.com>
2020-11-12 09:06:00 +0100

    changed text

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn something/i);
+  const linkElement = screen.getByText(/learn something!/i);
   expect(linkElement).toBeInTheDocument();
 });
